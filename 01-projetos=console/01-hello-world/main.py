f0e82913de811e4d2f68ed8c36e9fcd6f5fdc42e
arthursantosdealmeida168-ux <arthursantosdealmeida168@gmail.com>
2026-06-03 17:19:39 -0300

    Create main.py

---
+++ 01-projetos=console/01-hello-world/main.py
@@ -0,0 +1,8 @@
+print("Olá, mundo!") # Instrução de saída
+# Este é um comentario de linha única.
+# Ele explica o "porque" do código, não o "o quê"
+
+"""
+Este é um docstring
+Usado para documentar blocos de código maiores
+"""
